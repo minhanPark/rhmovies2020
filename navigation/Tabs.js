db03b2b399ad77522618e6f91cc7c77763124f36
minhan <alsgks1218@gmail.com>
2020-05-10 21:52:03 +0900

    completed Tab nav

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,5 +1,7 @@
 import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { Platform } from "react-native";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
@@ -18,7 +20,36 @@ export default ({ navigation, route }) => {
     });
   }, [route]);
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+          if (route.name === "Movies") {
+            iconName += "film";
+          } else if (route.name === "Tv") {
+            iconName += "tv";
+          } else if (route.name === "Search") {
+            iconName += "search";
+          } else if (route.name === "Favorite") {
+            iconName += "heart";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              color={focused ? "white" : "grey"}
+              size={26}
+            />
+          );
+        },
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: "black",
+          borderTopColor: "black",
+        },
+      }}
+    >
       <Tabs.Screen name="Movies" component={Movies} />
       <Tabs.Screen name="Tv" component={Tv} />
       <Tabs.Screen name="Search" component={Search} />
