06f28cc5a433e506f06841ddc927f30e6aed844a
minhan <alsgks1218@gmail.com>
2020-05-26 00:43:45 +0900

    search logic

---
+++ screens/Search/Container.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Presenter from "./Presenter";
+import { movieApi, tvApi } from "../../api";
+
+export default () => {
+  const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState({
+    movies: [],
+    shows: [],
+    movieError: null,
+    showsError: null,
+  });
+
+  const onChange = (text) => setKeyword(text);
+
+  const search = async () => {
+    const [movies, moviesError] = await movieApi.search(keyword);
+    const [shows, showsError] = await tvApi.search(keyword);
+    setResults({
+      movies,
+      shows,
+      moviesError,
+      showsError,
+    });
+  };
+  return (
+    <Presenter
+      {...results}
+      onChange={onChange}
+      onSubmit={search}
+      keyword={keyword}
+    />
+  );
+};
