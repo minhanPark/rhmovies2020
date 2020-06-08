a0e84560f9f91ad42b3216dcc659273b26d5c34c
minhan <alsgks1218@gmail.com>
2020-06-09 00:01:42 +0900

    add animation in discover page

--- screens/Favs/Container.js
+++ screens/Favs/Container.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
-import { movieApi } from "../api";
+import Presenter from "./Presenter";
+import { movieApi } from "../../api";
 
 export default () => {
   const [movies, setMovies] = useState({
@@ -17,9 +17,5 @@ export default () => {
   useEffect(() => {
     getData();
   }, []);
-  return (
-    <View>
-      <Text>{movies.results?.length}</Text>
-    </View>
-  );
+  return <Presenter {...movies} />;
 };
---
+++ screens/Favs/Presenter.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import styled from "styled-components/native";
+import { apiImage } from "../../api";
+import { Dimensions, PanResponder, Animated } from "react-native";
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+  align-items: center;
+`;
+
+const styles = {
+  top: 80,
+  height: HEIGHT / 1.5,
+  width: "90%",
+  position: "absolute",
+};
+
+const Poster = styled.Image`
+  border-radius: 20px;
+  width: 100%;
+  height: 100%;
+`;
+
+export default ({ results }) => {
+  const [topIndex, setTopIndex] = useState(0);
+  const position = new Animated.ValueXY();
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (evt, { dx, dy }) => {
+      position.setValue({ x: dx, y: dy });
+    },
+    onPanResponderRelease: () => {
+      Animated.spring(position, {
+        toValue: {
+          x: 0,
+          y: 0,
+        },
+      }).start();
+    },
+  });
+  const rotationValues = position.x.interpolate({
+    inputRange: [-100, 0, 100],
+    outputRange: ["-5deg", "0deg", "5deg"],
+    extrapolate: "clamp",
+  });
+  setInterval(() => {
+    console.log(rotationValues);
+  }, 500);
+  return (
+    <Container>
+      {results.map((result, index) => {
+        if (index === topIndex) {
+          return (
+            <Animated.View
+              key={result.id}
+              {...panResponder.panHandlers}
+              style={{
+                ...styles,
+                zIndex: 1,
+                transform: [
+                  { rotate: rotationValues },
+                  ...position.getTranslateTransform(),
+                ],
+              }}
+            >
+              <Poster source={{ uri: apiImage(result.poster_path) }} />
+            </Animated.View>
+          );
+        }
+        return (
+          <Animated.View
+            key={result.id}
+            {...panResponder.panHandlers}
+            style={{
+              ...styles,
+              zIndex: -index,
+            }}
+          >
+            <Poster source={{ uri: apiImage(result.poster_path) }} />
+          </Animated.View>
+        );
+      })}
+    </Container>
+  );
+};
